b10168d123bc9eab28559a25eb01ad27f1201cc3
Abdullah Waqas <abdullahwaqas@192.168.1.9>
2025-08-09 12:17:23 +0500

    removed changes

--- Home_Service/settings.py
+++ Home_Service/settings.py
@@ -1,14 +1,5 @@
 from pathlib import Path
 from decouple import Config, RepositoryEnv
-import os
-DEBUG = 'RENDER' not in os.environ
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME]
-else:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
 
 # Define the base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,8 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',   # <- add this right after SecurityMiddleware
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,9 +105,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
